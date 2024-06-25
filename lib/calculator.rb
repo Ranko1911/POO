b18af3ed62304c54c45aa0487a3eb9bdba150237
Ancor <64937197+Ranko1911@users.noreply.github.com>
2024-06-25 16:55:09 +0000

    Ejercicio TDD

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,3 +1,38 @@
 # lib/calculator.rb
 class Ejercicio
+  include Comparable 
+  include Enumerable
+  attr_reader :Identificador, :titulo, :materiales
+  def initialize(id = nil, titulo = nil, materiales = [])
+    @Identificador = id
+    @titulo = titulo
+    @materiales = materiales
+  end
+
+  def set_id(id)
+    @Identificador = id
+  end
+
+  def set_titulo(titulo)
+    @titulo = titulo
+  end
+
+  def set_materiales(materiales)
+    @materiales = materiales
+  end
+
+  def to_s
+    "#{@Identificador} #{@titulo} #{@materiales}"
+  end
+
+  def <=>(other)
+    raise typeError "Tipo de dato incorrecto" unless other.is_a? Ejercicio
+    @Identificador <=> other.Identificador
+  end
+
+  def each 
+    @materiales.each do |material|
+      yield material
+    end
+  end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,20 +2,73 @@
 require 'rspec'
 require_relative '../lib/calculator'
 
-# RSpec.describe Calculator do
-#   describe '#add' do
-#     it 'returns the sum of two numbers' do
-#       calculator = Calculator.new
-#       result = calculator.add(2, 3)
-#       expect(result).to eq(5)
-#     end
-#   end
-# end
 
-
-# frozen_string_literal: true
 RSpec.describe Ejercicio do
-  it "Iniciar clase vacía" do
-    expect(Ejercicio.new).to_not eq(nil)  
+  describe "Basic Work" do
+    it "Iniciar clase vacía" do
+      expect(Ejercicio.new).to_not be_nil
+    end
+    it "Test Identificador attribute" do
+      e = Ejercicio.new(1)
+      expect(e.Identificador).to eq(1)
+    end
+    it "Test Setter Identificador attribute" do
+      e = Ejercicio.new
+      e.set_id(1)
+      expect(e.Identificador).to eq(1)
+    end
+    it "Test titulo" do
+      e = Ejercicio.new(1,"primer")
+      expect(e.titulo).to eq("primer")
+    end
+    it "Test Setter titulo" do
+      e = Ejercicio.new
+      e.set_titulo("primer")
+      expect(e.titulo).to eq("primer")
+    end
+    it "Test de materiales" do
+      e1 = Ejercicio.new(1,"primer", [])
+      e2 = Ejercicio.new(2,"segundo", ["libro"])
+      e3 = Ejercicio.new(3, "tercero", ["tijeras", "pegamento"])
+
+      expect(e1.materiales).to eq([])
+      expect(e2.materiales).to eq(["libro"])
+      expect(e3.materiales).to eq(["tijeras", "pegamento"])
+    end
+    it "Test Setter Materiales" do
+      e1 = Ejercicio.new(1,"primer", [])
+      e1.set_materiales(["libro"])
+      expect(e1.materiales).to eq(["libro"])
+    end
+
+    it "Test To_s" do
+      e = Ejercicio.new(1,"primer", ["libro"])
+      expect(e.to_s).to eq("1 primer [\"libro\"]")
+    end
   end
+
+  describe "Comparable" do
+    it "Test Comparable" do
+      e1 = Ejercicio.new(1,"primer", [])
+      e2 = Ejercicio.new(2,"segundo", ["libro"])
+      e3 = Ejercicio.new(3, "tercero", ["tijeras", "pegamento"])
+
+      expect(e1 < e2).to eq(true)
+      expect(e2 < e3).to eq(true)
+      expect(e1 < e3).to eq(true)
+    end
+  end
+
+  describe "Enumerable" do
+    it "Test Enumerable" do
+      e = Ejercicio.new(1,"primer", ["libro", "tijeras", "pegamento"])
+      expect(e.map{|x| x.upcase}).to eq(["LIBRO", "TIJERAS", "PEGAMENTO"])
+      expect(e.select{|x| x.length > 5}).to eq(["tijeras", "pegamento"])
+      expect(e.reject{|x| x.length > 5}).to eq(["libro"])
+      expect(e.first).to eq("libro")
+      expect(e.max).to eq("tijeras")
+      expect(e.min).to eq("libro")
+    end
+  end
+
 end
